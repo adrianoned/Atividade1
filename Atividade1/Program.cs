7518ed29477bd7dbe5a2637a9ef283bf7c64a62a
Adriano Santos <adrianoned@hotmail.com>
2021-09-07 01:06:01 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Utilitario obj = new Utilitario();
+
+            #region Exercicio1_1
+            obj.calcularSeguencia();
+            #endregion
+
+            #region Exercicio1_2
+            int[] numeros = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+            //int[] numeros = { 1, 3, 5, 9, 13, 21, 35, 55, 89, 145 };
+            if (obj.verificaNumerosImpares(numeros))
+            {
+                Console.WriteLine("O Array possui apenas números impares!");
+            }
+            else
+                Console.WriteLine("O Array possui números pares!");
+            #endregion
+
+            #region Execicio1_3
+            obj.comparaArray();
+            #endregion
+            
+        }
+    }
+}
---
+++ Atividade1/Utilitario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    public class Utilitario
+    {
+        public void calcularSeguencia()
+        {
+            long qtdVezes;
+            long qtdVezesMaior = 0;
+            long numeroIncial = 0;
+            for (long i = 1; i <= 1000000; i++)
+            {
+                qtdVezes = 0;
+
+                for (long j = i; j > 1;)
+                {
+                    if (j % 2 == 0)
+                        j = (j / 2);
+                    else
+                        j = (3 * j + 1);
+
+                    qtdVezes++;
+                }
+
+                if(qtdVezesMaior < qtdVezes)
+                {
+                    qtdVezesMaior = qtdVezes;
+                    numeroIncial = i;
+                    
+                }
+                
+            }
+
+            Console.WriteLine($"A maior sequencia e iniciada pelo numero {numeroIncial} e efetuou {qtdVezesMaior} de iterações");
+        }
+
+       
+
+
+        public bool verificaNumerosImpares(int [] numeros)
+        {
+            return !(numeros.Where(obj => obj % 2 == 0).Any()) ;
+        }
+
+        public void comparaArray()
+        {
+            int[] primeiroArray = { 1, 3, 7, 29, 42, 98, 234, 93 };
+            int[] segundoArray = { 4, 6, 93, 7, 55, 32, 3 };
+
+            List<int> lst = new List<int>();
+            lst.AddRange(primeiroArray.Where(obj => !segundoArray.Contains(obj)));
+
+            Console.WriteLine($"Numeros que não estão contidos no segundo Array: {string.Join(",", lst.ToArray())}");
+        }
+
+    }
+
+    
+}
